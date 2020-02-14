1fe1ce638bad6640f00c8474be52d98addbe93f6
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-02-14 01:07:00 -0800

    [UPDATE] Have news articles display & scroll

--- lib/src/ui/home/home_screen.dart
+++ lib/src/ui/home/home_screen.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/src/bloc/home/home_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:news_app/src/model/category/category.dart';
+import 'package:news_app/src/model/category/top_headlines_news/response_top_headlines_news.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -278,14 +280,214 @@ class _WidgetLatestNewsState extends State<WidgetLatestNews> {
       DataState state, MediaQueryData mediaQuery) {
     if (state is DataLoading) {
       return Center(
-          child: Platform.isAndroid
-              ? CircularProgressIndicator()
-              : CupertinoActivityIndicator());
+        child: Platform.isAndroid
+            ? CircularProgressIndicator()
+            : CupertinoActivityIndicator(),
+      );
     } else if (state is DataSuccess) {
-      return Container(
-          child: Center(
-        child: Text('data success'),
-      ));
+      ResponseTopHeadlinesNews data = state.data;
+      return ListView.separated(
+        padding: EdgeInsets.zero,
+        itemCount: data.articles.length,
+        separatorBuilder: (context, index) {
+          return Divider();
+        },
+        itemBuilder: (context, index) {
+          Article itemArticle = data.articles[index];
+          if (index == 0) {
+            return Stack(
+              children: <Widget>[
+                ClipRRect(
+                  child: CachedNetworkImage(
+                    imageUrl: itemArticle.urlToImage,
+                    height: 192.0,
+                    width: mediaQuery.size.width,
+                    fit: BoxFit.cover,
+                    placeholder: (context, url) => Platform.isAndroid
+                        ? CircularProgressIndicator()
+                        : CupertinoActivityIndicator(),
+                    errorWidget: (context, url, error) => Image.asset(
+                      'assets/images/img_not_found.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8.0),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () async {
+//                   // TODO: do something in here
+                  },
+                  child: Container(
+                    width: mediaQuery.size.width,
+                    height: 192.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
+                      ),
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.black.withOpacity(0.8),
+                          Colors.black.withOpacity(0.0),
+                        ],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        stops: [
+                          0.0,
+                          0.7,
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 12.0,
+                        top: 12.0,
+                        right: 12.0,
+                      ),
+                      child: Text(
+                        itemArticle.title,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 12.0,
+                        top: 4.0,
+                        right: 12.0,
+                      ),
+                      child: Wrap(
+                        children: <Widget>[
+                          Icon(
+                            Icons.launch,
+                            color: Colors.white.withOpacity(0.8),
+                            size: 12.0,
+                          ),
+                          SizedBox(width: 4.0),
+                          Text(
+                            '${itemArticle.source.name}',
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.8),
+                              fontSize: 11.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            );
+          } else {
+            return GestureDetector(
+              onTap: () async {
+                // TODO: do something in here
+              },
+              child: Container(
+                width: mediaQuery.size.width,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                      child: SizedBox(
+                        height: 72.0,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              itemArticle.title,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                color: Color(0xFF325384),
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            Wrap(
+                              crossAxisAlignment: WrapCrossAlignment.center,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.launch,
+                                  size: 12.0,
+                                  color: Color(0xFF325384).withOpacity(0.5),
+                                ),
+                                SizedBox(width: 4.0),
+                                Text(
+                                  itemArticle.source.name,
+                                  style: TextStyle(
+                                    color: Color(0xFF325384).withOpacity(0.5),
+                                    fontSize: 12.0,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: ClipRRect(
+                        /*child: Image.network(
+                          itemArticle.urlToImage ??
+                              'http://api.bengkelrobot.net:8001/assets/images/img_not_found.jpg',
+                          width: 72.0,
+                          height: 72.0,
+                          fit: BoxFit.cover,
+                        ),*/
+                        child: CachedNetworkImage(
+                          imageUrl: itemArticle.urlToImage,
+                          imageBuilder: (context, imageProvider) {
+                            return Container(
+                              width: 72.0,
+                              height: 72.0,
+                              decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: imageProvider,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            );
+                          },
+                          placeholder: (context, url) => Container(
+                            width: 72.0,
+                            height: 72.0,
+                            child: Center(
+                              child: Platform.isAndroid
+                                  ? CircularProgressIndicator()
+                                  : CupertinoActivityIndicator(),
+                            ),
+                          ),
+                          errorWidget: (context, url, error) => Image.asset(
+                            'assets/images/img_not_found.jpg',
+                            fit: BoxFit.cover,
+                            width: 72.0,
+                            height: 72.0,
+                          ),
+                        ),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(4.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+        },
+      );
     } else {
       return Container();
     }
